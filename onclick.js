17f61bf35f71c5bd8fbf15a6fbb4770c9f3efe11
kelvinqdd <105149444+kelvinqdd@users.noreply.github.com>
2022-05-13 21:22:06 +0800

    Delete onclick.js

--- onclick.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("home").onmousedown = function () {
-  window.open("file:///C:/Users/dudu/Desktop/kelvinqdd.github.io/main.html",'_self').focus();
-  return true
-};
-document.getElementById("tech").onmousedown = function () {
-  window.open("file:///C:/Users/dudu/Desktop/kelvinqdd.github.io/main.html#tech",'_self').focus();
-  return true
-};
